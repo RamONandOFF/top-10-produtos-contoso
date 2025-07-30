7ea27e129e35014eb4c6abeb47146210632312c0
Ramon Ribeiro <133510629+RamONandOFF@users.noreply.github.com>
2025-07-30 10:40:30 -0300

    Add files via upload

---
+++ consulta_top_10_produtos.sql
@@ -0,0 +1,14 @@
+-- Consulta para identificar os 10 produtos com maior volume de vendas no banco Contoso
+
+SELECT TOP 10 
+    p.ProductName AS Nome_Produto
+	,SUM(F.SalesQuantity) AS Quantidade_Vendida
+	,FORMAT(SUM(F.SalesAmount), 'C0') AS Receita_Total
+FROM FactSales F
+
+JOIN DimProduct P ON F.ProductKey = P.ProductKey
+
+GROUP BY P.ProductName
+
+ORDER BY Quantidade_Vendida DESC
+
